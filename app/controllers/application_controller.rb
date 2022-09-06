ec795612c7eb980025d36ae0c9cd45f3e4abe9b7
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-06 18:25:24 +0300

    CRUD operations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,51 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/book" do
+    book = Book.all
+    book.to_json
+  end
+  # get book by id
+  get "/book/:id" do
+    book = Book.find(params[:id])
+    book.to_json
+  end
+  #get reviews
+  get "/reviews" do
+    reviews = Review.all
+    reviews.to_json
+  end
+  #get author details
+  get "/author" do
+    author = Author.all
+    author.to_json
   end
 
+  post "/book" do
+    books = Book.create(
+      title: params[:title],
+      price: params[:price],
+      publisher: params[:publisher],
+      year: params[:year],
+    )
+    books.to_json
+  end
+
+  # update 
+  patch "/book/:id" do
+    books = Book.find(params[:id])
+    books.update(
+      title: params[:title],
+      publisher: params[:publisher],
+    )
+    books.to_json
+  end
+
+  #delete
+  delete "/book/:id" do
+    books = Book.find(params[:id])
+    books.destroy
+    books.to_json
+  end
 end
